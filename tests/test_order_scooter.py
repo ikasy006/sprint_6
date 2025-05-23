cd5e4f7e1fb04130cc5320f16d8bf43b047ea3b7
Cerque <fedor20060609@mail.ru>
2025-05-23 16:24:37 +0300

    v1

--- tests/test_order_scooter.py
+++ tests/test_order_scooter.py
@@ -3,12 +3,6 @@ import pytest
 from PageObjects.pages.main_page import MainPage
 from PageObjects.pages.order_scooter import OrderScooter
 
-
-test_data = [
-    ['Рина', 'Кесс', 'москва', 'Черкизовская', '+79002223311', '23.05.2025', 'сутки', 'чёрный жемчуг'],
-    ['Алия', 'Джесс', 'Москва, Варшавское шоссе', 'Севастопольская', '89997776644', '30.05.2025', 'двое суток', 'серая безысходность']
-]
-
 def test_order_scooter_for_header_button(main_page):
     main_page.go_to_site()
     order_scooter_page = OrderScooter(main_page.click_to_header_button())
